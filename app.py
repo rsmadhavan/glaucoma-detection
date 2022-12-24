8ebab289266aa4e0faf594c28f44fbddab632fa4
Madhavan Saravanamurthi <60268967+rsmadhavan@users.noreply.github.com>
2022-12-24 12:19:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,82 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+import cv2
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/model_final.h5'
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+#model._make_predict_function()              # Necessary
+# print('Model loaded. Start serving...')
+
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#model.save('')
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = cv2.imread(img_path)
+    img = np.expand_dims(img,axis=0)
+    pred = model.predict(img)
+    class_names = ['negative', 'positive']
+    pred_class = class_names[np.argmax(pred)]
+    conf = round(100 * (np.max(pred)), 2)
+    # img = image.load_img(img_path, target_size=(224, 224))
+    return pred_class,conf 
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        print(file_path)
+        f.save(file_path)
+
+        # Make prediction
+        pred = model_predict(file_path, model)
+
+        # Process your result for human
+        # pred_class = preds.argmax(axis=-1)            # Simple argmax
+        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        result = f"Predicted {pred[0]} at conf {pred[1]} "         # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
